e5f604a944e553045aef73a2033d3177f5a65311
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-27 20:43:40 -0500

    Update project

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const productsApi = require('./routes/product');
-const receiptsApi = require('./routes/receipt');
+const productsApi = require("./routes/product");
+const receiptsApi = require("./routes/receipt");
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   let userInfo = req.header("user-agent");
-  res.send(`New UserInfo: ${userInfo}`);
+  res.send(`UserInfo: ${userInfo}`);
 });
 
 productsApi(app);
 receiptsApi(app);
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
     console.error("Error: ", err);
     return;
